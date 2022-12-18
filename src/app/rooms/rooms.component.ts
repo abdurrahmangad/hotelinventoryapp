82ff64f4e4175a840fb7014986ff9950bc2914cd
abd-elrahman <abg@luftborn.com>
2022-12-18 17:05:47 +0200

    built-directives

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Room, RoomList} from './rooms';
 
 @Component({
   selector: 'app-rooms',
@@ -12,6 +13,58 @@ export class RoomsComponent {
   hideRooms:boolean = false;
 
 
+  rooms : Room = {
+    availableRooms:0,
+    bookedRooms:5,
+    totalRooms:15  
+  };
+
+  roomList: RoomList[] =[
+    {
+      roomNumber:0,
+      roomType:'room ',
+      amenities:'test ',
+      photos:'test.jpg',
+      price: 134,
+      checkinTime: new Date('11-Nov-2015'),
+      checkoutTime: new Date('11-Nov-2015')
+    },
+    {
+      roomNumber:1,
+      roomType:'room ',
+      amenities:'test ',
+      photos:'test.jpg',
+      price: 134,
+      checkinTime: new Date('11-Nov-2015'),
+      checkoutTime: new Date('11-Nov-2015')
+    },
+    {
+      roomNumber:2,
+      roomType:'room ',
+      amenities:'test ',
+      photos:'test.jpg',
+      price: 134,
+      checkinTime: new Date('11-Nov-2015'),
+      checkoutTime: new Date('11-Nov-2015')
+    },
+    {
+      roomNumber:3,
+      roomType:'room ',
+      amenities:'test ',
+      photos:'test.jpg',
+      price: 134,
+      checkinTime: new Date('11-Nov-2015'),
+      checkoutTime: new Date('11-Nov-2015')
+    }
+
+  ]
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
  toggle(){
   this.hideRooms = !this.hideRooms;
  } 
